348b9fd108047bceee587c1e2c5f213f93347a45
morphling <MorphlingDev@hotmail.com>
2024-11-22 15:07:39 +0800

    add github link icon / fetch usage count in page

---
+++ src/_lib/db/data.ts
@@ -0,0 +1,19 @@
+
+import { prisma } from "@/_lib/db/client";
+
+async function genSHA256Hash(input:string) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(input);
+    const hashBuffer = crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(await hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+const llmApiKey = process.env.GOOGLE_GENERATIVE_AI_API_KEY?? "";
+const hashedApiKey = await genSHA256Hash(llmApiKey);
+
+export async function fetchApiUsageCount() {
+    const apiCountQyeryData = await prisma.apiCount.findUnique({where:{hashAPIKey:hashedApiKey}});
+    return apiCountQyeryData?.usageCount?? -1;
+}
