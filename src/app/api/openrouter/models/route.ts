ce64592bd7cd66543ef912d56e884d618cc22e12
morphling <MorphlingDev@hotmail.com>
2024-12-12 00:45:34 +0800

    use openrouter api

---
+++ src/app/api/openrouter/models/route.ts
@@ -0,0 +1,16 @@
+
+export const maxDuration = 30;
+
+const baseUrl = process.env["OPENROUTER_API_URL"];
+
+export async function GET() {
+
+    const response = await fetch(`${baseUrl}/models`);
+    if (response.status === 200) {
+        const data = await response.json();
+        for (const model of data["data"]) {
+            console.log("model name: "+model["name"]);
+        }
+    }
+
+}
